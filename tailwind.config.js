fda5cb0d839dc14d5dc014531b075164cedae22a
maszz <maszzari2005@gmail.com>
2022-08-24 11:16:48 +0800

    Perbaikan 24/08/2022

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}", "./src/*.{html, js}"],
+	content: ["./src/**/*.{html,js}", "./src/*.{html, js}", "./index.{html, js}"],
 	theme: {
 		extend: {
 			colors: {
